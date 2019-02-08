149348f600cd7b3ad57b7346107f79860ab6f143
Philip Andrew <philip@contractpen.com>
2019-02-08 11:19:43 +0800

    working state

--- src/test/scala/scalaExcel/MyTest.scala
+++ src/test/scala/scalaExcel/MyTest.scala
@@ -10,6 +10,7 @@ import scalaExcel.model.{DefaultValueFormat, Styles}
 class MyTest {
 
   @Test def testDefaultFormat() {
+    println("in MyTest")
     val style = Styles.DEFAULT.setFormat(DefaultValueFormat)
     assertEquals("123.46", style.format(VDouble(123.4567)))
   }
