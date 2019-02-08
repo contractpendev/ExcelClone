87c30c14ef2c4fca5d4304d5f5a467b460df7e34
Philip Andrew <philip@contractpen.com>
2019-02-08 11:36:44 +0800

    demo excel

--- src/test/scala/scalaExcel/MyTest.scala
+++ src/test/scala/scalaExcel/MyTest.scala
@@ -5,14 +5,20 @@ import java.util.Locale
 import scalaExcel.formula._
 import org.junit.Assert._
 import org.junit._
+import scalaExcel.model.immutable.Sheet
 import scalaExcel.model.{DefaultValueFormat, Styles}
 
 class MyTest {
 
   @Test def testDefaultFormat() {
-    println("in MyTest")
-    val style = Styles.DEFAULT.setFormat(DefaultValueFormat)
-    assertEquals("123.46", style.format(VDouble(123.4567)))
+
+    val sheet = new Sheet()
+    val a = sheet.setCell( (0,0), "100")
+    val b = a.setCell( (0,1), "=A1")
+    assert(b.getValue( (0,1) ) == VDouble(100.0))
+
+    //val style = Styles.DEFAULT.setFormat(DefaultValueFormat)
+    //assertEquals("123.46", style.format(VDouble(123.4567)))
   }
 
 }
