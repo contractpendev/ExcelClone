ce1af0fa164a26f540f038cd7f89bf78715f06a5
Philip Andrew <philip@contractpen.com>
2019-02-13 13:30:45 +0800

    Excel to Ergo code

--- src/test/scala/scalaExcel/MyTest.scala
+++ src/test/scala/scalaExcel/MyTest.scala
@@ -5,23 +5,244 @@ import java.util.Locale
 import scalaExcel.formula._
 import org.junit.Assert._
 import org.junit._
+import scalaExcel.ergo.ExcelToErgo
+import scalaExcel.ergo.ExcelToErgo._
 import scalaExcel.model.immutable.Sheet
 import scalaExcel.model.{DefaultValueFormat, Styles}
 
+import scala.collection.mutable
+
 class MyTest {
+
+  def testSpreadsheets(): Seq[IOSheet] = {
+
+    val sheets = mutable.Buffer[Sheet]()
+    val inputs = mutable.Buffer[Seq[CellInput]]()
+    val outputs = mutable.Buffer[Seq[CellReference]]()
+
+    sheets += new Sheet()
+      .setCell( (0,0), "100")
+      .setCell( (0,1), "50")
+      .setCell( (0,2), "=A1+A2")
+
+    inputs += Seq(CellInput("name1", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")),
+      CellInput("name2", CellReference(DataTypeDouble, 0, 1), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 0, 2))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "1")
+      .setCell((1, 0), "=2+2/0.5+(1.2/2.0)")
+      .setCell((0, 1), "=A1+1")
+      .setCell((1, 1), "=$A$1+1")
+      .setCell((3, 0), "=B1/0.5")
+
+    inputs += Seq(CellInput("name1", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")),
+      CellInput("name2", CellReference(DataTypeDouble, 1, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 3, 0))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "1")
+      .setCell((1, 0), "2")
+      .setCell((0, 1), "=A1*1")
+      .copyCell((0, 1), (1, 1))
+
+    inputs += Seq(CellInput("name1", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")),
+      CellInput("name2", CellReference(DataTypeDouble, 1, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 1, 1))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "0")
+      .setCell((0, 1), "2")
+      .setCell((0, 2), "=A1+A2") // 1
+      .setCell((0, 3), "=A2*A3") // 2
+      .setCell((0, 4), "=A3/A4") // 3
+      .setCell((0, 5), "=A4+A5") // 5
+
+    inputs += Seq(CellInput("name1", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")),
+      CellInput("name2", CellReference(DataTypeDouble, 0, 1), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 0, 5))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "100")
+      .setCell((0, 1), "2")
+      .setCell((0, 2), "=A1^A2")
+      .setCell((0, 3), "=A3")
+
+    inputs += Seq(CellInput("name1", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "100")
+      .setCell((0, 1), "101")
+      .setCell((0, 2), "=A1 <> A2")
+      .setCell((0, 3), "=A3")
+
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=95.2%")
+      .setCell((0, 1), "=1000.0")
+      .setCell((0, 2), "=A1*A2")
+
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+
+    outputs += Seq(CellReference(DataTypeDouble, 0, 2))
+
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=SUM (A1,A2,A3)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=AVERAGE(A1,A2,A3)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=3")
+      .setCell((0, 1), "=2")
+      .setCell((0, 3), "=POWER(A1,A2)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1.058329234")
+      .setCell((0, 1), "=2")
+      .setCell((0, 3), "=ROUND(A1,A2)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=ROW(A2)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=ROWS(A1:A3)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=COLUMN(C3)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=COLUMNS(C3:D5)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+    /* @todo count
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=COUNT(C3:D5)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))*/
+
+    sheets += new Sheet()
+      .setCell((0, 0), "=1")
+      .setCell((0, 1), "=2")
+      .setCell((0, 2), "=3")
+      .setCell((0, 3), "=MATCH(C3:D5)")
+    inputs += Seq(CellInput("name", CellReference(DataTypeDouble, 0, 0), DataModelFieldReference("fieldName", "ClassName", "package.name")))
+    outputs += Seq(CellReference(DataTypeDouble, 0, 3))
+
+
+
+    /*
+      case "SUM"     => reduce(ctx, applyToDoubles(_ + _), VDouble(0), desugarArgs(args))
+      case "AVERAGE" => evalCallAverage(ctx, desugarArgs(args))
+      case "POWER"   => evalCallPower(ctx, args)
+      case "ROUND"   => evalCallRound(ctx, args)
+      case "ROW"     => evalCallRow(args)
+      case "ROWS"    => evalCallRows(args)
+      case "COLUMN"  => evalCallColumn(args)
+      case "COLUMNS" => evalCallColumns(args)
+      case "COUNT"   => evalCallCount(ctx, desugarArgs(args))
+      case "MATCH"   => evalCallMatch(ctx, args)
+      case "VLOOKUP" => evalCallVLookUp(ctx, args)
+      case "ADDRESS" => evalCallAddress(ctx, args)
+      case "IF"      => evalCallIf(ctx, args)
+      case "OR"      => evalCallOr(ctx, desugarArgs(args))
+      case "AND"     => evalCallAnd(ctx, desugarArgs(args))
+      case "NOT"     => evalCallNot(ctx, args)
+      case "UPPER"   => evalCallUpper(ctx, args)
+      case "LOWER"   => evalCallLower(ctx, args)
+      case "LEN"     => evalCallLen(ctx, args)
+      case "TRIM"    => evalCallTrim(ctx, args)
+      case "TRUNC"   => evalCallTrunc(ctx, args)
+      case "ISBLANK"   => evalCallIsBlank(ctx, desugarArgs(args))
+      case "ISERROR"   => evalCallIsError(ctx, desugarArgs(args))
+      case "ISNA"      => evalCallIsNA(ctx, desugarArgs(args))
+      case "ISLOGICAL" => evalCallIsLogical(ctx, desugarArgs(args))
+      case "ISNUMBER"  => evalCallIsNumber(ctx, desugarArgs(args))
+      case "ISTEXT"    => evalCallIsText(ctx, desugarArgs(args))
+     */
+    // ##############################################################################################################
+
+    val inputs2 = inputs.toSeq
+    val outputs2 = outputs.toSeq
+
+    val z = sheets.toSeq
+    for (s <- z.zip(inputs2).zip(outputs2).zipWithIndex) yield { IOSheet(s"sheet${s._2}", s._1._1._2, s._1._2, s._1._1._1) }
+  }
+
+  def nameFromCell(sheetName: String, out: CellReference): String = {
+    s"${sheetName}CellC${out.cellCol}R${out.cellRow}"
+  }
 
   @Test def testDefaultFormat() {
 
-    val sheet = new Sheet()
-    val sheet1 = sheet.setCell( (0,0), "100")
-    val sheet2 = sheet1.setCell( (0,1), "50")
-    val sheet3 = sheet2.setCell( (0,2), "=A1+A2")
-    println(pprint.apply(sheet3.getCell(0, 2).AST))
+    val t = ExcelToErgo.getType("1")
+    println(t)
 
-    assert(sheet3.getValue( (0,2) ) == VDouble(150.0))
-
-    //val style = Styles.DEFAULT.setFormat(DefaultValueFormat)
-    //assertEquals("123.46", style.format(VDouble(123.4567)))
+    val sheets = testSpreadsheets()
+    for (sheet <- sheets) {
+      println("")
+      println("Sheet " + sheet.name + " ---------------------------------------------------------------------------------------")
+      for (out <- sheet.output) {
+        val ast = sheet.sheet.getCell(out.cellCol, out.cellRow).AST
+        val all = Seq(Binding(nameFromCell(sheet.name, out), out.cellCol, out.cellRow, ast))
+        val ergo = ExcelToErgo.toErgoCode(sheet.sheet, Bindings(all), Seq.empty)
+        for (e <- ergo.lines) {
+          println(e.line)
+        }
+        println(sheet.sheet.getValue((out.cellCol, out.cellRow)))
+      }
+    }
+    println("")
   }
 
 }
---
+++ src/test/scala/scalaExcel/ergo/ExcelToErgo.scala
@@ -0,0 +1,410 @@
+package scalaExcel.ergo
+
+import com.sun.org.apache.xpath.internal.operations.NotEquals
+import scalaExcel.formula.Evaluator.Ctx
+import scalaExcel.formula._
+import scalaExcel.model.Sheet
+
+object ExcelToErgo {
+
+  val FUNCTION_SUM = "SUM"
+  val FUNCTION_AVERAGE = "AVERAGE"
+  val FUNCTION_POWER = "POWER"
+  val FUNCTION_ROUND = "ROUND"
+  val FUNCTION_ROW = "ROW"
+  val FUNCTION_ROWS = "ROWS"
+  val FUNCTION_COLUMN = "COLUMN"
+  val FUNCTION_COLUMNS = "COLUMNS"
+  val FUNCTION_COUNT = "COUNT"
+  val FUNCTION_MATCH = "MATCH"
+  val FUNCTION_VLOOKUP = "VLOOKUP"
+  val FUNCTION_ADDRESS = "ADDRESS"
+  val FUNCTION_IF = "IF"
+  val FUNCTION_OR = "OR"
+  val FUNCTION_AND = "AND"
+  val FUNCTION_NOT = "NOT"
+  val FUNCTION_UPPER = "UPPER"
+  val FUNCTION_LOWER = "LOWER"
+  val FUNCTION_LEN = "LEN"
+  val FUNCTION_TRIM = "TRIM"
+  val FUNCTION_TRUNC = "TRUNC"
+  val FUNCTION_ISBLANK = "ISBLANK"
+  val FUNCTION_ISERROR = "ISERROR"
+  val FUNCTION_ISNA = "ISNA"
+  val FUNCTION_ISLOGICAL = "ISLOGICAL"
+  val FUNCTION_IISNUMBER = "ISNUMBER"
+  val FUNCTION_ISTEXT = "ISTEXT"
+
+  /*
+  case "SUM"     => reduce(ctx, applyToDoubles(_ + _), VDouble(0), desugarArgs(args))
+  case "AVERAGE" => evalCallAverage(ctx, desugarArgs(args))
+  case "POWER"   => evalCallPower(ctx, args)
+  case "ROUND"   => evalCallRound(ctx, args)
+  case "ROW"     => evalCallRow(args)
+  case "ROWS"    => evalCallRows(args)
+  case "COLUMN"  => evalCallColumn(args)
+  case "COLUMNS" => evalCallColumns(args)
+  case "COUNT"   => evalCallCount(ctx, desugarArgs(args))
+  case "MATCH"   => evalCallMatch(ctx, args)
+  case "VLOOKUP" => evalCallVLookUp(ctx, args)
+  case "ADDRESS" => evalCallAddress(ctx, args)
+  case "IF"      => evalCallIf(ctx, args)
+  case "OR"      => evalCallOr(ctx, desugarArgs(args))
+  case "AND"     => evalCallAnd(ctx, desugarArgs(args))
+  case "NOT"     => evalCallNot(ctx, args)
+  case "UPPER"   => evalCallUpper(ctx, args)
+  case "LOWER"   => evalCallLower(ctx, args)
+  case "LEN"     => evalCallLen(ctx, args)
+  case "TRIM"    => evalCallTrim(ctx, args)
+  case "TRUNC"   => evalCallTrunc(ctx, args)
+  case "ISBLANK"   => evalCallIsBlank(ctx, desugarArgs(args))
+  case "ISERROR"   => evalCallIsError(ctx, desugarArgs(args))
+  case "ISNA"      => evalCallIsNA(ctx, desugarArgs(args))
+  case "ISLOGICAL" => evalCallIsLogical(ctx, desugarArgs(args))
+  case "ISNUMBER"  => evalCallIsNumber(ctx, desugarArgs(args))
+  case "ISTEXT"    => evalCallIsText(ctx, desugarArgs(args))
+ */
+
+  sealed class ErgoExpression
+
+  case class ErgoInputName(name: String) extends ErgoExpression
+  sealed class OperationType extends ErgoExpression
+
+  object Sum extends OperationType
+  object Neg extends OperationType
+  object Mult extends OperationType
+  object Divide extends OperationType
+  object Equals extends OperationType
+  object GreaterThan extends OperationType
+  object LessThan extends OperationType
+  object GreaterThanOrEquals extends OperationType
+  object LessThanOrEquals extends OperationType
+  object NoEquals extends OperationType
+  object Concatenate extends OperationType
+  object Exponent extends OperationType
+  object Percentage extends OperationType
+
+  case class Expression1(operationType: OperationType, left: ErgoExpression) extends ErgoExpression
+  case class Expression2(operationType: OperationType, left: ErgoExpression, right: ErgoExpression) extends ErgoExpression
+  case class ErgoNumber(number: Double = 0.0d) extends ErgoExpression
+  case class ErgoString(valueOf: String = "") extends ErgoExpression
+  case class FunctionCall(functionName: String = "", parameters: Seq[ErgoExpression]) extends ErgoExpression
+  case class Nothing() extends ErgoExpression
+
+  case class ErgoLine(line: String)
+  case class Ergo(lines: Seq[ErgoLine])
+
+  // A binding to a cell in a spreadsheet has a variable name
+  case class Binding(variableName: String, cellCol: Int, cellRow: Int, ast: Expr)
+  case class Bindings(bindings: Seq[Binding])
+
+  case class Spreadsheet(bindingsOut: Bindings)
+
+  // eg. let name : String = "Hello";
+  // let x : Double = 3.1416;
+  case class LetBinding(name: String, dataType: DataType, expression: ErgoExpression)
+
+  sealed class DataType
+  object DataTypeDouble extends DataType
+  object DataTypeInt extends DataType
+  object DataTypeString extends DataType
+
+  // A reference to a data model field
+  case class DataModelFieldReference(modelFieldName: String, modelFieldClassName: String, modelFieldNamespace: String)
+  // Reference in a spreadsheet
+  case class CellReference(typeOf: DataType, cellCol: Int, cellRow: Int)
+  case class CellInput(name: String, cellReference: CellReference, modelFieldReference: DataModelFieldReference)
+
+  case class IOSheet(name: String, input: Seq[CellInput], output: Seq[CellReference], sheet: Sheet)
+
+  case class ErgoCode(code: String)
+
+  def percentToDoubleString(code: String): String = {
+    // Assume code is a number
+    val d = (code.toDouble * 0.01d)
+    "%1.4f".format(d)
+  }
+
+  private def ergoTreeToCode(tree: ErgoExpression, inputs: Seq[CellInput]): ErgoCode = {
+    tree match {
+      case num: ErgoNumber => ErgoCode(s"${num.number}")
+      case Expression2(opType: OperationType, left, right) => {
+        val op = opType match {
+          case Sum => "+"
+          case Neg => "-"
+          case Mult => "*"
+          case Divide => "/"
+          case Exponent => "^"
+          case Equals => "=="
+          case NoEquals => "!="
+          case z: Object => {
+            println("unhandled case: " + z)
+            ""
+          }
+        }
+        val a = for (r <- Seq(left, right)) yield ergoTreeToCode(r, inputs)
+        ErgoCode(s"(${a(0).code}) $op (${a(1).code})")
+      }
+      case Expression1(opType: OperationType, left) => {
+        val code = ergoTreeToCode(left, inputs)
+        opType match {
+          case Percentage => ErgoCode(percentToDoubleString(code.code))
+          case z: Object => {
+            println("Unhandled case " + z)
+            ErgoCode("")
+          }
+        }
+      }
+      case ErgoInputName(name: String) => {
+        ErgoCode(name)
+      }
+      case FunctionCall(functionName, parameters: Seq[ErgoExpression]) => {
+        val parametersCode = for (p <- parameters) yield {
+          val code = ergoTreeToCode(p, inputs).code
+          s"($code)"
+        }
+        functionName match {
+          case FUNCTION_SUM => ErgoCode(parametersCode.mkString(" + "))
+          case FUNCTION_AVERAGE => {
+            val commaSep = parametersCode.mkString(", ")
+            ErgoCode(s"average([$commaSep])")
+          }
+          case FUNCTION_POWER => {
+            val sep = parametersCode.mkString(" ^ ")
+            ErgoCode(sep)
+          }
+          /*@todo Unknown so far...
+          case FUNCTION_ROUND => {
+            val sep = parametersCode.mkString(" ^ ")
+            ErgoCode(sep)
+          }*/
+          case FUNCTION_ROUND => {
+            ErgoCode("The case is not handled yet")
+          }
+          case FUNCTION_ROW => {
+            ErgoCode(parametersCode.head)
+          }
+          case FUNCTION_ROWS => {
+            ErgoCode(parametersCode.head)
+          }
+          case FUNCTION_COLUMN => {
+            ErgoCode(parametersCode.head)
+          }
+          case FUNCTION_COLUMNS => {
+            ErgoCode(parametersCode.head)
+          }
+          case _ => {
+            ErgoCode("This case is not handled yet")
+          }
+        }
+      }
+      case other: Object => {
+        println("unhandled case 55: " + other)
+        ErgoCode("")
+      }
+    }
+  }
+
+  case class CellWrap(name: Option[String], cell: Option[scalaExcel.model.Cell])
+
+  private def getCellWrap(sheet: Sheet, inputs: Seq[CellInput], col: Int, row: Int): CellWrap = {
+    val f = inputs.find(c => (c.cellReference.cellCol == col) && (c.cellReference.cellRow == row))
+    if (f.isDefined)
+      CellWrap(Some(f.get.name), None)
+    else
+      CellWrap(None, Some(sheet.getCell((col, row))))
+  }
+
+  private def cellWrapToErgoTree(sheet: Sheet, cellWrap: CellWrap, inputs: Seq[CellInput]): ErgoExpression = {
+    if (cellWrap.cell.isDefined)
+      cellWrapToErgoTree(sheet, cellWrap.cell.get.AST, inputs)
+    else
+      ErgoInputName(cellWrap.name.get)
+  }
+
+  def evalRowRange(sheet: Sheet, e1Rows: Range, inputs: Seq[CellInput]): ErgoExpression = {
+    val start = e1Rows.start
+    val end = e1Rows.end
+    println("Start: " + start.r.referent)
+    println("End: " + end.r.referent)
+    println("Diff: " + Math.abs(start.r.referent - end.r.referent) + 1)
+    ErgoNumber(Math.abs(start.r.referent - end.r.referent) + 1)
+  }
+
+  def evalColRange(sheet: Sheet, e1Rows: Range, inputs: Seq[CellInput]): ErgoExpression = {
+    val start = e1Rows.start
+    val end = e1Rows.end
+    ErgoNumber(Math.abs(start.c.referent - end.c.referent) + 1)
+  }
+
+
+  def evalMatchFunction(function: String, sheet: Sheet, e1Rows: Range, inputs: Seq[CellInput]): ErgoExpression = {
+    //val start = e1Rows.start
+    //val end = e1Rows.end
+
+    //Evaluator.evalCall(ctx: Ctx, function, args: List[Expr])
+
+    ErgoNumber(0)
+  }
+
+  def cellOrOther(e1: Expr, sheet: Sheet, inputs: Seq[CellInput], functionCall: String = ""): ErgoExpression = {
+    e1 match {
+      case e1Cell: Cell => cellWrapToErgoTree(sheet, getCellWrap(sheet, inputs, e1Cell.c.referent, e1Cell.r.referent), inputs)
+      case e1Const: Const => fromValue(e1Const.v)
+      case e1Value: Value => fromValue(e1Value)
+      // BinOp(Plus(),Const(VDouble(2.0)),BinOp(Div(),Const(VDouble(2.0)),Const(VDouble(0.5))))
+      case e1BinOp: BinOp => cellWrapToErgoTree(sheet, e1BinOp, inputs)
+      //Group(BinOp(Div(),Const(VDouble(1.2)),Const(VDouble(2.0))))
+      case e1Group: Group => cellWrapToErgoTree(sheet, e1Group.e, inputs)
+      case r1Range: Range => {
+        functionCall match {
+          case FUNCTION_ROWS => evalRowRange(sheet, r1Range, inputs)
+          case FUNCTION_COLUMNS => evalColRange(sheet, r1Range, inputs)
+          case FUNCTION_MATCH => evalMatchFunction(FUNCTION_MATCH, sheet, r1Range, inputs)
+          case other: Object => {
+            println("unhandled case lala")
+            println(other)
+            null
+          }
+        }
+      }
+      case e1What: Object => println("Unhandled case z: " + e1What); null
+    }
+  }
+
+  private def cellWrapToErgoTree(sheet: Sheet, ast: Expr, inputs: Seq[CellInput]): ErgoExpression = {
+
+    def commonality(op: Op, opType: OperationType, e1: Expr, e2: Expr) = {
+      Expression2(opType, cellOrOther(e1, sheet, inputs), cellOrOther(e2, sheet, inputs))
+    }
+
+    def commonality2(op: Op, opType: OperationType, e1: Expr) = {
+      Expression1(opType, cellOrOther(e1, sheet, inputs))
+    }
+
+    //println(ast)
+    ast match {
+      // 1 + 1
+      case BinOp(Plus(), e1: Expr, e2: Expr) => commonality(Plus(), Sum, e1, e2)
+      // 1 - 1
+      case BinOp(Minus(), e1: Expr, e2: Expr) => commonality(Minus(), Neg, e1, e2)
+      // 1 * 1
+      case BinOp(Mul(), e1: Expr, e2: Expr) => commonality(Mul(), Mult, e1, e2)
+      // 1 / 1
+      case BinOp(Div(), e1: Expr, e2: Expr) => commonality(Div(), Divide, e1, e2)
+      // 1 == 1
+      case BinOp(Eq(), e1: Expr, e2: Expr) => commonality(Eq(), Equals, e1, e2)
+      // 1 > 1
+      case BinOp(GT(), e1: Expr, e2: Expr) => commonality(GT(), GreaterThan, e1, e2)
+      // 1 < 1
+      case BinOp(LT(), e1: Expr, e2: Expr) => commonality(LT(), LessThan, e1, e2)
+      // 1 >= 1
+      case BinOp(GTE(), e1: Expr, e2: Expr) => commonality(GTE(), GreaterThanOrEquals, e1, e2)
+      // 1 <= 1
+      case BinOp(LTE(), e1: Expr, e2: Expr) => commonality(LTE(), LessThanOrEquals, e1, e2)
+      // 1 != 1
+      case BinOp(NEq(), e1: Expr, e2: Expr) => commonality(NEq(), NoEquals, e1, e2)
+      // Concatenate ?
+      case BinOp(Concat(), e1: Expr, e2: Expr) => commonality(Concat(), Concatenate, e1, e2)
+      // 5 ^ 2
+      case BinOp(Expon(), e1: Expr, e2: Expr) => commonality(Expon(), Exponent, e1, e2)
+      // 10%
+      case UnOp(Percent(), e1: Expr) => commonality2(Percent(), Percentage, e1)
+
+      case Cell(ColRef(col: Int,false), RowRef(row: Int,false)) => {
+        cellWrapToErgoTree(sheet, getCellWrap(sheet, inputs, col, row), inputs)
+      }
+
+      case Const(v: VDouble) => {
+        ErgoNumber(v.d)
+      }
+      case Const(v: Any) => {
+        if (v==VEmpty) {
+          println(v)
+          println("In this case return empty string? or error?")
+          ErgoNumber(0.0d)
+        } else {
+          println("What is this case")
+          println(v)
+          ErgoNumber(0.0d)
+        }
+      }
+      case UnOp(Percent(), e1: Expr) => commonality2(Percent(), Percentage, e1)
+
+      case Call(FUNCTION_ROWS, cells: List[Expr]) => {
+        cells.head match {
+          case e1Rows: Range => evalRowRange(sheet, e1Rows, inputs)
+          case other: Object => {
+            println("unhandled case 95: " + other)
+            null
+          }
+        }
+      }
+      case Call(f, cells: List[Expr]) => {
+        val parameters = for (c <- cells) yield cellOrOther(c, sheet, inputs, f)
+        FunctionCall(f, parameters)
+      }
+
+      case other: Any => {
+        println("Unhandled case 2: " + other)
+        Nothing()
+      }
+    }
+  }
+
+
+
+
+  // Returns type of a string as excel code interprets
+  def getType(s: String) = {
+    def removeParentheses(e : Expr) : Expr = e match {
+      case Group(e1) => removeParentheses(e1)
+      case UnOp(op, e1) => UnOp(op, removeParentheses(e1))
+      case BinOp(op, e1, e2) => BinOp(op, removeParentheses(e1), removeParentheses(e2))
+      case Call(f, args) => Call(f, args.map(removeParentheses))
+      case other: Any => {
+        //println("Unhandled case 3: " + other)
+        e
+      }
+    }
+    val parsed = removeParentheses(Parser parsing s)
+    parsed
+  }
+
+  def fromValue(value: Value): ErgoExpression = {
+    value match {
+      case d: VDouble => ErgoNumber(d.d)
+      case s: VString => ErgoString(s.s)
+      case other: Value => ErgoNumber(0.0f)
+      case other: Any => {
+        println("Unhandled case 4: " + other)
+        ErgoNumber(0.0f)
+      }
+    }
+  }
+
+  def toErgoLine(sheet: Sheet, ast: Expr, inputs: Seq[CellInput]): ErgoLine = {
+    val tree = cellWrapToErgoTree(sheet, ast, inputs)
+    ErgoLine(ergoTreeToCode(tree, inputs).code)
+  }
+
+  def toErgoCode(sheet: Sheet, bindings: Bindings, inputs: Seq[CellInput]): Ergo = {
+
+    /*val prefix: Seq[ErgoLine] = {
+      for (p <- Seq("define function average(parameters : Array[Double]) : Double {",
+        " code here }", "")) yield ErgoLine(p)
+    }*/
+
+    val lines: Seq[ErgoLine] = for (b <- bindings.bindings) yield {
+      val v = ergoTreeToCode(cellWrapToErgoTree(sheet, b.ast, inputs), inputs)
+      ErgoLine(s"  let ${b.variableName} : Double = ${v.code};")
+    }
+    val lines2: Seq[ErgoLine] = ErgoLine("define function area(radius : Double) : Double {") +: lines
+    val lines3: Seq[ErgoLine] = lines2 :+ ErgoLine("}")
+
+    //val lines4: Seq[ErgoLine] = prefix ++ lines3
+
+    Ergo(lines3)
+  }
+
+}
